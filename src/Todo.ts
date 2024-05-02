fce5973a9397e49f16e360608021b02ab9e5f886
Dhruviika <E21CSEU0292@bennett.edu.in>
2024-05-02 23:33:11 +0530

    added routes to update todo and delete todo.

--- src/Todo.ts
+++ src/Todo.ts
@@ -4,8 +4,10 @@ import express from "express";
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.post("/", async (req, res) => {
-  const { userId, description } = req.body;
+//create todo
+router.post("/:userId", async (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const description = req.body.description;
   await prisma.todo.create({
     data: {
       userId,
@@ -15,6 +17,7 @@ router.post("/", async (req, res) => {
   res.send("Todo created");
 });
 
+//get all todos of user
 router.get("/:userId", async (req, res) => {
   const userId = parseInt(req.params.userId);
   const todos = await prisma.todo.findMany({
@@ -23,6 +26,53 @@ router.get("/:userId", async (req, res) => {
     },
   });
   res.json(todos);
+});
+
+//update todo
+router.put("/:todoId", async (req, res) => {
+  const todoId = parseInt(req.params.todoId);
+  const userId = parseInt(req.body.userId);
+  const { description } = req.body;
+  const completed = Boolean(req.body.completed);
+
+  if (description) {
+    await prisma.todo.update({
+      where: {
+        id: todoId,
+        userId: userId,
+      },
+      data: {
+        description,
+      },
+    });
+  }
+
+  if (completed !== undefined) {
+    await prisma.todo.update({
+      where: {
+        id: todoId,
+        userId: userId,
+      },
+      data: {
+        completed: completed,
+      },
+    });
+  }
+
+  res.send("Todo updated");
+});
+
+//delete todo
+router.delete("/:todoId", async (req, res) => {
+  const todoId = parseInt(req.params.todoId);
+  const userId = parseInt(req.body.userId);
+  await prisma.todo.delete({
+    where: {
+      id: todoId,
+      userId: userId,
+    },
+  });
+  res.send("Todo deleted");
 });
 
 export default router;
