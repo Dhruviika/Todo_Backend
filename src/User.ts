17c496668afdcc917a3d4941bd511cdb14a8b30d
Dhruviika <E21CSEU0292@bennett.edu.in>
2024-05-02 23:31:37 +0530

    Added a route to get all users and to get user by id.

--- src/User.ts
+++ src/User.ts
@@ -5,27 +5,67 @@ import express from "express";
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.post("/", async (req, res) => {
-  const { name, username, password } = req.body;
+//get all users
+router.get("/all", async (req, res) => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      firstName: true,
+      username: true,
+    },
+  });
+  res.json(users);
+});
 
-  await prisma.user.create({
+//create new user
+router.post("/", async (req, res) => {
+  const { firstName, username, password } = req.body;
+
+  const user = await prisma.user.create({
     data: {
       username,
       password,
-      firstName: name,
+      firstName,
     },
   });
-  res.send("User created");
+  res.send({ user: user, message: "User created" });
 });
 
+//user login
 router.get("/", async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.query;
+
+  if (typeof username !== "string" || typeof password !== "string") {
+    return res.status(400).send("Invalid query parameters");
+  }
+
   const user = await prisma.user.findUnique({
     where: {
       username,
       password,
     },
     select: {
+      id: true,
+      firstName: true,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+
+  res.json(user);
+});
+
+//get user by id
+router.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
       firstName: true,
       todos: {
         select: {
